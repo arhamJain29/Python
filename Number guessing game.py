e0dc53152700ac6b8ff41f8e612ee93db1d3961e
arhamJain29 <arhamjainkhater@gmail.com>
2025-01-02 22:58:20 +0530

    added Number guessing game

---
+++ Number guessing game.py
@@ -0,0 +1,29 @@
+import random
+
+def random_number():
+    i = 0
+    lower = int(input('enter the lower limit of the range: \n'))
+    upper = int(input("enter the upper limit of the range: \n"))
+
+    Range  = {'low' : lower, 'high' :upper }
+    print(f'Range is:  {Range.values()}')
+
+    Guessing_number = random.randint(lower,upper)
+    Guessed_Correctly = True
+    while Guessed_Correctly == True:
+        Guessed_number = int(input('Enter the Number you Guessed: \n'))
+        if Guessed_number > Guessing_number:
+            print('Try Again !! You have Guessed Very High')
+            i += 1
+        elif Guessed_number < Guessing_number:
+            print('Try Again !! You have Guessed Very Low')
+            i += 1
+        elif Guessed_number == Guessing_number:
+            Guessed_Correctly = False
+            i += 1
+
+    print('Woohoh !! You have Guessed correctly')
+    print(i)
+
+
+random_number()
