6620e668f0b71df072fcc50e17b963ab4bb72ebb
arhamJain29 <arhamjainkhater@gmail.com>
2025-01-09 00:12:20 +0530

    modified the game

--- follower_comparing_game.py/main.py
+++ follower_comparing_game.py/main.py
@@ -6,9 +6,12 @@ from art import vs
 def assign():
     return random.choice(data)
 
-def compare(p1 , p2):
+def compare():
     p1 = assign()
     p2 = assign()
+
+    while p1 == p2:
+        p2 = assign()
 
     v1 = p1['follower_count']
     v2 = p2['follower_count']
@@ -17,44 +20,46 @@ def compare(p1 , p2):
 
     if v1 > v2:
         max = p1['name']
-        
     elif v2 > v1:
         max = p2['name']
+    
+    return p1, p2, max
 
 def play():
 
+    score = 0
 
-    while want_to_play == 'y':
-        
-        compare(p1 , p2)
+    while True:
+        p1, p2, max = compare()
 
         print(logo) 
-        
-        print(f' Name: {(p1['name'])}, description: {p1['description']}, country :{p1['country']}  {vs}  Name: {(p2['name'])}, description: {p2['description']}, country :{p2['country']}')
+        print(f"Name: {p1['name']}, description: {p1['description']}, country: {p1['country']}")
+        print(vs)
+        print(f"Name: {p2['name']}, description: {p2['description']}, country: {p2['country']}")
 
-        score = 0
-        user_input = input('Guess which one has more follower')
+        user_input = input('Guess which one has more followers: ')
 
         if user_input == max:
             print('Perfect Guess !! Woohoo')
-            score+=1
+            score += 1
+            break
         else:
-            print('Wrong guess!!') 
-            score = score
+            print('Wrong guess!!')
+            break
 
+    print(f'Your final score is {score}')
 
+def main():
+    while True:
+        want_to_play = input("Do you want to play (y/n): \n").lower()
+        
+        if want_to_play == 'y':
+            play()
+        elif want_to_play == 'n':
+            print('Thank you, Exiting the game.')
+            break
+        else:
+            print('Invalid input, enter a valid input')
 
-
-    
-    
-while True:
-    want_to_play = input("Do you want to play (y/n): \n").lower()
-    if want_to_play == 'y':
-        play()
-    elif want_to_play == 'n':
-        print('Thank you, Exiting the game.')
-        False
-    else:
-        print('Invalid input, enter a valid input')
-        True
-
+if __name__ == "__main__":
+    main()
